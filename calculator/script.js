ee8820873a7e80b90b6dcd4eb6624503fa1b993e
PURAB PRATIK <pratikpurab@gmail.com>
2022-11-01 10:40:41 +0530

    1st project

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('num'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        //console.log(e.target.innerText);
+        switch (e.target.innerText) {
+            case 'CLEAR':
+                display.value = '';
+                break;
+            case 'DEL':
+                display.value = display.value.slice(0, -1);
+                break;
+            case '=':
+                try {
+                    display.value = eval(display.value);
+                } catch {
+                    alert("Error!")
+                }
+                break;
+            default:
+                
+                display.value += e.target.innerText;
+        }
+    });
+});
